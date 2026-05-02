b959bd0c3b7fccff0378d2eb2660de252c81bad1
CourtWatchJamaica <courtwatchjamaica@protonmail.com>
2026-05-02 06:20:04 +0000

    v2 provider update

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { CourtProvider } from "@/lib/court-context";
+import { ChambersProvider } from "@/lib/chambers-context";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      {children}
+      <CourtProvider>
+        <ChambersProvider>{children}</ChambersProvider>
+      </CourtProvider>
     </ThemeProvider>
   );
 }
