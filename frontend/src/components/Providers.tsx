b1e50611c7fa4d768ffd9576a1fd7e8da85da112
CourtWatchJamaica <courtwatchjamaica@protonmail.com>
2026-05-04 04:52:53 +0000

    v3 --update providers

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from "next-themes";
 import { CourtProvider } from "@/lib/court-context";
 import { ChambersProvider } from "@/lib/chambers-context";
 import { TrackingProvider } from "@/lib/tracking-context";
+import MaintenanceGate from "@/components/MaintenanceGate";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <CourtProvider>
         <ChambersProvider>
-          <TrackingProvider>{children}</TrackingProvider>
+          <TrackingProvider>
+            <MaintenanceGate>{children}</MaintenanceGate>
+          </TrackingProvider>
         </ChambersProvider>
       </CourtProvider>
     </ThemeProvider>
